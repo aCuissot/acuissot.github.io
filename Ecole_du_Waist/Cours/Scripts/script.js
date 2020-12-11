9125e460eaa813b3146b881426e7e1e569eca17d
aCuissot <harrypotter9752@gmail.com>
2020-12-11 21:27:45 +0100

    refactor

---
+++ Ecole_du_Waist/Cours/Scripts/script.js
@@ -0,0 +1,47 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+// Je sais je peux joindre ces 3 fonctions mais pour le moment c'est pas ma priorité
+
+function funcCours(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-red";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-red", "");
+    }
+}
+
+function funcEC(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+
+function funcEx(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-blue";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-blue", "");
+    }
+}
