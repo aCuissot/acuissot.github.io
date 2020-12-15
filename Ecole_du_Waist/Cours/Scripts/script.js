f4ed501fa78c29fa80253663c84cd7ae4054444b
aCuissot <harrypotter9752@gmail.com>
2020-12-15 15:45:01 +0100

    adding exercices -> V1.0 du site de l'école

--- Ecole_du_Waist/Cours/Scripts/script.js
+++ Ecole_du_Waist/Cours/Scripts/script.js
@@ -45,3 +45,12 @@ function funcEx(id) {
             x.previousElementSibling.className.replace(" w3-blue", "");
     }
 }
+
+function funcShowSolution(id){
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
