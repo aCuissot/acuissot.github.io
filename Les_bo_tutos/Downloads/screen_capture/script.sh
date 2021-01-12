064d5c7e52b68417bffcdbff7e58a40d66ce540c
aCuissot <harrypotter9752@gmail.com>
2021-01-12 20:20:33 +0100

    avancées csgo tips + modif screen cap + ajout acces bO depuis index

--- Les_bo_tutos/Downloads/screen_capture/script.sh
+++ Les_bo_tutos/Downloads/screen_capture/script.sh
@@ -3,10 +3,18 @@
 # Default values
 path=$HOME"/Videos/MyCaptures"
 mkdir -p $path
-maxTime=3
+maxTime=3600
 name="mycapture"
 doThePythonProcess=false
 extension=".jpg"
+
+trap createVideo SIGINT
+createVideo() {
+   if $doThePythonProcess
+   then python script.py -i $path -p $name -o $name".mp4" -l $((SECONDS-start))
+   fi
+   exit
+}
 
 # Get the options flags
 while getopts t:p:f:F:P option
@@ -27,6 +35,8 @@ while ((maxTime > ((SECONDS - start))));
 do gnome-screenshot -f $path"/"$name"-"$count$extension;
 count=$(($count+1));
 done
+
+echo "fin de la capture"
 
 if $doThePythonProcess
 then python script.py -i $path -p $name -o $name".mp4" -l $((SECONDS-start))
